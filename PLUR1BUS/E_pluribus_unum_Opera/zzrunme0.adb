e4e998f17bda516df30dfbb60db720660687d1db
GreatSealUSA <circlebird12@gmail.com>
2025-12-26 11:01:47 -0700

    Create zzrunme0.adb
    untested Ada app, wanted the GitHuib project to show more than just BASIC language. 
    
    ==============
    
    The First Mandate (June 1983): Under Secretary of Defense Dr. Richard DeLauer issued a memorandum mandating Ada for all new "mission-critical" computer procurements starting in 1984.
    
    Formal Policy (DODD 3405.1 - 1987): This directive, titled "Computer Programming Language Policy," officially designated Ada as the single common language for defense computer resources used in intelligence, command and control, and weapon systems.
    
    Structure and Waivers (DODD 3405.2 - 1987): Titled "Ada in Weapon Systems," this established the roles of the Ada Executive Official and the Waiver Control Officer to manage exceptions where using another language could be proven more cost-effective.

---
+++ PLUR1BUS/E_pluribus_unum_Opera/zzrunme0.adb
@@ -0,0 +1,16 @@
+-- Ada code
+-- License: Public Domain, no rights reserved.
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Wake_Loop is
+begin
+   -- Original Commodore 64 hack to set lowercase
+   -- (POKE 53272,22) has no direct equivalent in modern Ada
+   -- and is omitted here.
+
+   -- Main loop: repeatedly print the message
+   loop
+      Put_Line ("Finnegans Wake From Page 628 jump to Page 3");
+   end loop;
+end Wake_Loop;
