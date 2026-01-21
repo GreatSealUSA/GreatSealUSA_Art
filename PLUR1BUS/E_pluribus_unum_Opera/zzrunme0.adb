401540d4eb0bcf83d0950730c7988acfcedfc314
GreatSealUSA <circlebird12@gmail.com>
2026-01-21 09:28:42 -0700

    Creative Mythology
    Creative Mythology is Volume IV of the comparative mythologist Joseph Campbell's The Masks of God. The book concerns "creative mythology", Campbell's term for the efforts by an individual to communicate his experience through signs, an attempt that can become "living myth".
    
    Year 1968
    Year before my birth
    Campbell did his honeymoon with the dancing student in Woodstock, before it became world famous town in 1969!
    
    https://en.wikipedia.org/wiki/Creative_Mythology

--- PLUR1BUS/E_pluribus_unum_Opera/zzrunme0.adb
+++ PLUR1BUS/E_pluribus_unum_Opera/zzrunme0.adb
@@ -2,6 +2,14 @@
 --   https://EN.Wikipedia.org/wiki/Ada_(programming_language)
 -- License: Public Domain, no rights reserved.
 -- Application version 0.0.13
+--
+-- ToDo: can I get this to run on a Commodore 128 CP/M system?
+-- ToDo: can I get this to run on an Apple //+ Microsoft CP/M system?
+-- "Several Ada compilers were developed for the historic CP/M operating system, ...
+--   primarily in the 1980s. The two most notable commercial compilers were Janus/Ada ...
+--   and SuperSoft Ada, while a more modern, active option for enthusiasts is the ...
+--   experimental Cowgol compiler."
+--   Google Search Summary 2026-01-21 morning USA
 
 with Ada.Text_IO; use Ada.Text_IO;
 
