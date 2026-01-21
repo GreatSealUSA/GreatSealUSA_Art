da17b7d5d93465259aa61f7626c800866c037593
GreatSealUSA <circlebird12@gmail.com>
2026-01-21 09:30:46 -0700

    Microsoft hardware for Apple

--- PLUR1BUS/E_pluribus_unum_Opera/zzrunme0.adb
+++ PLUR1BUS/E_pluribus_unum_Opera/zzrunme0.adb
@@ -3,8 +3,9 @@
 -- License: Public Domain, no rights reserved.
 -- Application version 0.0.13
 --
--- ToDo: can I get this to run on a Commodore 128 CP/M system?
--- ToDo: can I get this to run on an Apple //+ Microsoft CP/M system?
+-- ToDo: can I get this app to run on a Commodore 128 CP/M system?
+-- ToDo: can I get this app to run on an Apple //+ Microsoft CP/M system?
+--   https://en.wikipedia.org/wiki/Z-80_SoftCard
 -- "Several Ada compilers were developed for the historic CP/M operating system, ...
 --   primarily in the 1980s. The two most notable commercial compilers were Janus/Ada ...
 --   and SuperSoft Ada, while a more modern, active option for enthusiasts is the ...
